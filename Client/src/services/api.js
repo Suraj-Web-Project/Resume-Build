6fe4458c98b06d3ddb0505c74d1cadc830d853c5
Suraj Nayak <surajnayak1304@gmail.com>
2026-03-10 21:24:32 +0530

    Update API base URL to production endpoint

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: "https://resume-build-vfd2.onrender.com/api",
 });
 
 export default api;
